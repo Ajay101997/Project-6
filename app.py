84d9397b719cec670b69090d55cc2bd2ec513c2c
Ajay Kumar Sahu <ajaykumarsahu2017.1997@gmail.com>
2026-06-23 01:30:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Load model
+model=pickle.load(open("model.pkl","rb"))
+st.title("Rossmann Sales Prediction app")
+st.write("Enter store details to predict sales")
+
+# User input
+day_of_week=st.selectbox("Day of Week",[1,2,3,4,5,6,7])
+promo=st.selectbox("Promo",[0,1])
+school_holiday=st.selectbox("School Holiday",[0,1])
+competition_distance=st.number_input("Competition Distance",min_value=0.0)
+month=st.number_input("Month",1,12)
+day=st.number_input("Day",1,31)
+year=st.number_input("Year",2013,2015)
+
+# Store Type
+store_type=st.selectbox("Store Type",["a","b","c","d"])
+
+# Assortment
+assortment=st.selectbox("Assortment",["a","b","c"])
+
+### Convert into encoded format
+# StoreType encoding
+storetype_b=1 if store_type=="b" else 0
+storetype_c=1 if store_type=="c" else 0
+storetype_d=1 if store_type=="d" else 0
+# Assortment encoding
+assortment_b=1 if assortment=="b" else 0
+assortment_c=1 if assortment=="c" else 0
+
+# Feature engineering
+is_weekend=day_of_week in [6,7]
+is_month_start=(day<=3)
+is_month_end=(day>=28)
+
+#Create input dataframe
+inputDf=pd.DataFrame({"DayOfWeek":[day_of_week],"Open":[1],"Promo":[promo],"SchoolHoliday":[school_holiday],"CompetitionDistance":competition_distance,
+                      "CompetitionOpenSinceMonth":[0],"CompetitionOpenSinceYear":[0],"Promo2":[0],"Promo2SinceWeek":[0],"Promo2SinceYear":[0],
+                      "Year":[year],"Month":[month],"Day":[day],"IsWeekend":[is_weekend],"IsMonthStart":[is_month_start],"IsMonthEnd":[is_month_end],
+                     "StoreType_b":[storetype_b],"StoreType_c":[storetype_c],"StoreType_d":[storetype_d],"Assortment_b":[assortment_b],
+                      "Assortment_c":[assortment_c]})
+
+# Align features
+train_columns=model.feature_names_in_
+inputDf=inputDf.reindex(columns=train_columns,fill_value=0)
+
+# Prediction
+if st.button("Predict Sales"):
+    prediction=model.predict(inputDf)
+    st.success(f"Predicted Sales:{prediction[0]:.2f}")
